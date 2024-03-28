c22b7cdba9f968c3b13c1dd598322fbf050799cb
Jordan Maynor <mail@jtm.li>
2024-03-28 16:02:06 -0500

    Initial work on adding sharepoint REST API.
    May not be correct, haven't gotten into documentation yet. But may need this API in addition to the main graph API because when getting info from sharepoint lists, the People columns seem to just be a lookup ID. A lookup ID that can't be translated to the actual user info (email and/or name) with just the graph API.

--- grafap.py
+++ grafap.py
@@ -12,13 +12,14 @@ import requests
 
 class Decorators:
     """
-    Decorators class
+    Decorators class for handling token refreshing
+    for Microsoft Graph and Sharepoint Rest API
     """
 
     @staticmethod
-    def refresh_token(decorated):
+    def refresh_graph_token(decorated):
         """
-        Decorator to refresh the access token if it has expired
+        Decorator to refresh the graph access token if it has expired
         """
 
         def wrapper(*args, **kwargs):
@@ -33,14 +34,38 @@ class Decorators:
                 "GRAPH_BEARER_TOKEN" not in os.environ
                 or datetime.strptime(expires_at, "%m/%d/%Y %H:%M:%S") < datetime.now()
             ):
-                Decorators.get_token()
+                Decorators.get_graph_token()
             return decorated(*args, **kwargs)
 
         wrapper.__name__ = decorated.__name__
         return wrapper
 
     @staticmethod
-    def get_token():
+    def refresh_sp_token(decorated):
+        """
+        Decorator to refresh the sharepoint rest API access token if it has expired
+        """
+
+        def wrapper(*args, **kwargs):
+            """
+            Wrapper function
+            """
+            if "SP_BEARER_TOKEN_EXPIRES_AT" not in os.environ:
+                expires_at = "01/01/1901 00:00:00"
+            else:
+                expires_at = os.environ["SP_BEARER_TOKEN_EXPIRES_AT"]
+            if (
+                "SP_BEARER_TOKEN" not in os.environ
+                or datetime.strptime(expires_at, "%m/%d/%Y %H:%M:%S") < datetime.now()
+            ):
+                Decorators.get_sp_token()
+            return decorated(*args, **kwargs)
+
+        wrapper.__name__ = decorated.__name__
+        return wrapper
+
+    @staticmethod
+    def get_graph_token():
         """
         Get Microsoft Graph bearer token
         """
@@ -90,8 +115,61 @@ class Decorators:
             print("Error, could not set os env expires at: ", e)
             raise Exception("Error, could not set os env expires at: " + str(e))
 
+    @staticmethod
+    def get_sp_token():
+        """
+        Gets Sharepoint Rest API bearer token.
+        """
+        if "SP_LOGIN_BASE_URL" not in os.environ:
+            raise Exception("Error, could not find SP_LOGIN_BASE_URL in env")
+        if "SP_TENANT_ID" not in os.environ:
+            raise Exception("Error, could not find SP_TENANT_ID in env")
+        if "SP_CLIENT_ID" not in os.environ:
+            raise Exception("Error, could not find SP_CLIENT_ID in env")
+        if "SP_CLIENT_SECRET" not in os.environ:
+            raise Exception("Error, could not find SP_CLIENT_SECRET in env")
+        if "SP_GRANT_TYPE" not in os.environ:
+            raise Exception("Error, could not find SP_GRANT_TYPE in env")
+        if "SP_SITE" not in os.environ:
+            raise Exception("Error, could not find SP_SITE in env")
 
-@Decorators.refresh_token
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+
+        response = requests.post(
+            os.environ["SP_LOGIN_BASE_URL"]
+            + os.environ["SP_TENANT_ID"]
+            + "/oauth2/v2.0/token",
+            headers=headers,
+            data={
+                "client_id": os.environ["SP_CLIENT_ID"],
+                "client_secret": os.environ["SP_CLIENT_SECRET"],
+                "grant_type": "client_credentials",
+                "resource": os.environ["SP_SITE"],
+            },
+            timeout=30,
+        )
+        try:
+            os.environ["SP_BEARER_TOKEN"] = response.json()["access_token"]
+        except Exception as e:
+            print("Error, could not set OS env bearer token: ", e)
+            print(response.content)
+            raise Exception("Error, could not set OS env bearer token: " + str(e))
+        try:
+            expires_at = datetime.now() + timedelta(
+                seconds=response.json()["expires_in"]
+            )
+            os.environ["SP_BEARER_TOKEN_EXPIRES_AT"] = expires_at.strftime(
+                "%m/%d/%Y %H:%M:%S"
+            )
+        except Exception as e:
+            print("Error, could not set os env expires at: ", e)
+            raise Exception("Error, could not set os env expires at: " + str(e))
+
+
+@Decorators.refresh_graph_token
 def get_sp_sites() -> dict:
     """
     Gets all site data in a given tenant
@@ -126,7 +204,7 @@ def get_sp_sites() -> dict:
     return result
 
 
-@Decorators.refresh_token
+@Decorators.refresh_graph_token
 def get_sp_lists(siteid: str) -> dict:
     """
     Gets all lists in a given site
@@ -162,11 +240,11 @@ def get_sp_lists(siteid: str) -> dict:
     return result
 
 
-@Decorators.refresh_token
-def get_sp_list_items(siteid: str, listid: str) -> dict:
+@Decorators.refresh_graph_token
+def get_sp_list_items(siteid: str, listid: str, filter_query: str = None) -> dict:
     """
     Gets field data from a sharepoint list
-    TODO: Add support for filtering
+    filter_query is an optional OData filter query
     """
 
     if "GRAPH_BASE_URL" not in os.environ:
@@ -192,19 +270,26 @@ def get_sp_list_items(siteid: str, listid: str) -> dict:
         else:
             return data["value"]
 
-    result = recurs_get(
+    url = (
         os.environ["GRAPH_BASE_URL"]
         + siteid
         + "/lists/"
         + listid
-        + "/items?expand=fields",
+        + "/items?expand=fields"
+    )
+
+    if filter_query:
+        url += "&$filter=" + filter_query
+
+    result = recurs_get(
+        url,
         headers={"Authorization": "Bearer " + os.environ["GRAPH_BEARER_TOKEN"]},
     )
 
     return result
 
 
-@Decorators.refresh_token
+@Decorators.refresh_graph_token
 def create_sp_item(siteid: str, listid: str, field_data: dict):
     """
     Create a new item in SharePoint
@@ -226,7 +311,7 @@ def create_sp_item(siteid: str, listid: str, field_data: dict):
         raise Exception("Error, could not create item in sharepoint: " + str(e))
 
 
-@Decorators.refresh_token
+@Decorators.refresh_graph_token
 def update_sp_item(siteid: str, listid: str, item_id: str, field_data: dict[str, str]):
     """
     Update an item in SharePoint
