ae585471b1bee126b8991743242130d54297cdef
Jordan Maynor <mail@jtm.li>
2024-09-27 09:28:07 -0500

    Tenacity added to termstore.py

--- grafap/termstore.py
+++ grafap/termstore.py
@@ -3,8 +3,10 @@ import os
 import requests
 
 from grafap._auth import Decorators
+from grafap._helpers import _basic_retry
 
 
+@_basic_retry
 @Decorators._refresh_graph_token
 def get_sp_termstore_groups(site_id: str) -> dict:
     """
@@ -15,15 +17,18 @@ def get_sp_termstore_groups(site_id: str) -> dict:
     if "GRAPH_BASE_URL" not in os.environ:
         raise Exception("Error, could not find GRAPH_BASE_URL in env")
 
-    response = requests.get(
-        os.environ["GRAPH_BASE_URL"] + site_id + "/termStore/groups",
-        headers={"Authorization": "Bearer " + os.environ["GRAPH_BEARER_TOKEN"]},
-        timeout=30,
-    )
-
-    if response.status_code != 200:
-        raise Exception(
-            f"Error {response.status_code}, could not get termstore groups: {response.content}"
+    try:
+        response = requests.get(
+            os.environ["GRAPH_BASE_URL"] + site_id + "/termStore/groups",
+            headers={"Authorization": "Bearer " + os.environ["GRAPH_BEARER_TOKEN"]},
+            timeout=30,
         )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        raise Exception(
+            f"Error {e.response.status_code}, could not get termstore groups: {e}"
+        )
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error, could not get termstore groups: {e}")
 
     return response.json()
