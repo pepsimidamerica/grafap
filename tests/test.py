b5d2775cf7be636c27e1b9b661e8325b42d7bd2a
Jordan Maynor <mail@jtm.li>
2024-09-27 09:45:59 -0500

    Uncommented

--- tests/test.py
+++ tests/test.py
@@ -20,9 +20,11 @@ pass
 
 # # SharePoint Lists
 
-# lists = grafap.get_sp_lists(sites[0]["id"])
-# list_items = grafap.get_sp_list_items(sites[0]["id"], lists[0]["id"])
-# list_item = grafap.get_sp_list_item(sites[0]["id"], lists[0]["id"], list_items[0]["id"])
+lists = grafap.get_sp_lists(sites[0]["id"])
+list_items = grafap.get_sp_list_items(sites[0]["id"], lists[0]["id"])
+list_item = grafap.get_sp_list_item(sites[0]["id"], lists[0]["id"], list_items[0]["id"])
+
+pass
 
 # grafap.create_sp_item(
 #     sites[0]["id"],
